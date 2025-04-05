c1e5d9bb0f6374bbbe3e2371d58b8cb0fde9232c
andrejmanin <andrejmanin54@gmail.com>
2025-04-05 13:20:21 +0300

    Added try, catch

--- TodoAPI/Endpoints/TodoEndpoints.cs
+++ TodoAPI/Endpoints/TodoEndpoints.cs
@@ -11,63 +11,102 @@ public static class TodoEndpoints
     {
         builder.MapGet("/todo-list", async (SqlConnectionFactory sqlConnectionFactory) =>
         {
-            using var connection = sqlConnectionFactory.Create();
-            const string sql = "SELECT * FROM dbo.[Todo];";
-
-            var todoes = await connection.QueryAsync<Todo>(sql);
-            return Results.Ok(todoes);
+            try
+            {
+                using var connection = sqlConnectionFactory.Create();
+                const string sql = "SELECT * FROM dbo.[Todo];";
+                var todos = await connection.QueryAsync<Todo>(sql);
+                return Results.Ok(todos);
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         });
 
         builder.MapGet("/todo-list/{id}", async (int id, SqlConnectionFactory sqlConnectionFactory) =>
         {
-            using var connection = sqlConnectionFactory.Create();
-            const string sql = @"
-                SELECT * FROM dbo.[Todo]
-                WHERE Id = @TodoId;
-            ";
+            try
+            {
+                using var connection = sqlConnectionFactory.Create();
+                const string sql = @"
+                    SELECT * FROM dbo.[Todo]
+                    WHERE Id = @TodoId;
+                ";
 
-            var todoes = await connection.QuerySingleOrDefaultAsync<Todo>(sql, new { TodoId = id });
-            return todoes is not null ? Results.Ok(todoes) : Results.NotFound();
+                var todoes = await connection.QuerySingleOrDefaultAsync<Todo>(sql, new { TodoId = id });
+                return todoes is not null ? Results.Ok(todoes) : Results.NotFound();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         });
 
         builder.MapPost("/todo-list", async (Todo todo, SqlConnectionFactory sqlConnectionFactory) =>
         {
-            using var connection = sqlConnectionFactory.Create();
-            const string sql = @"
-                INSERT INTO Todo (Title, Description, IsComplete) 
-                VALUES (@Title, @Description, @IsComplete);
-            ";
-            await connection.ExecuteAsync(sql, todo);
-            return Results.Ok();
+            try
+            {
+                using var connection = sqlConnectionFactory.Create();
+                const string sql = @"
+                    INSERT INTO Todo (Title, Description, IsComplete) 
+                    VALUES (@Title, @Description, @IsComplete);
+                ";
+                await connection.ExecuteAsync(sql, todo);
+                return Results.Ok();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         });
 
         builder.MapPut("/todo-list", async (Todo todo, SqlConnectionFactory sqlConnectionFactory) =>
         {
-            using var connection = sqlConnectionFactory.Create();
-            string sql;
-            if (todo.IsComplete == 0)
+            try
             {
-                 sql = @"
-                    UPDATE Todo 
-                    SET Title = @Title, Description = @Description, IsComplete = @IsComplete
-                    WHERE Id = @Id;
-                ";
-                await connection.ExecuteAsync(sql, todo);   
+                using var connection = sqlConnectionFactory.Create();
+                string sql;
+                if (todo.IsComplete == 0)
+                {
+                    sql = @"
+                        UPDATE Todo 
+                        SET Title = @Title, Description = @Description, IsComplete = @IsComplete
+                        WHERE Id = @Id;
+                    ";
+                    await connection.ExecuteAsync(sql, todo);   
+                }
+                else
+                {
+                    sql = "DELETE FROM Todo WHERE Id = @Id;";
+                    await connection.ExecuteAsync(sql, new { Id = todo.Id });
+                }
+                return Results.Ok();
             }
-            else
+            catch (SqlException e)
             {
-                sql = "DELETE FROM Todo WHERE Id = @Id;";
-                await connection.ExecuteAsync(sql, new { Id = todo.Id });
+                Console.WriteLine(e);
+                throw;
             }
-            return Results.Ok();
         });
 
         builder.MapDelete("/todo-list/{id}", async (int id, SqlConnectionFactory sqlConnectionFactory) =>
         {
-            using var connection = sqlConnectionFactory.Create();
-            const string sql = "DELETE FROM Todo WHERE Id = @Id;";
-            await connection.ExecuteAsync(sql, new { Id = id });
-            return Results.Ok();
+            try
+            {
+                using var connection = sqlConnectionFactory.Create();
+                const string sql = "DELETE FROM Todo WHERE Id = @Id;";
+                await connection.ExecuteAsync(sql, new { Id = id });
+                return Results.Ok();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         });
     }
 }
